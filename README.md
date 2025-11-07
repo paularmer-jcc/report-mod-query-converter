33e0851d18836b3c9c4ca5bbc12f879f2494761a
Paul Armer <paul.armer@jud.ca.gov>
2025-11-07 11:13:15 -0800

    handle nested sql statements

--- README.md
+++ README.md
@@ -9,4 +9,5 @@
 1. Run the script: `npm run start`
     - If the query has variables in it, you will be prompted to input a new name for them
     - Suggested names are provided in `(var_name)`. To accept the suggestion, simply hit enter
+    - Variable names that have the word `date` in them will be wrapped in `to_date(:var_name, 'mm/dd/yyyy')`
 1. The formatted sql will be inserted into your clipboard for you to paste into Power BI
--- index.js
+++ index.js
@@ -3,67 +3,92 @@ import readline from 'node:readline';
 import { format as sqlFormat } from 'sql-formatter';
 import { copy } from 'copy-paste/promises.js';
 
-const SOURCE_FILE = './source.sql';
-console.log(`reading sql from '${SOURCE_FILE}'`);
-let sql = readFileSync(SOURCE_FILE, 'utf8').replace(/;/g, '');
+try {
+    let sql = readSource();
+    sql = await replaceVariables(sql);
+    sql = aliasColumns(sql);
+    copyToClipboard(sql);
+} catch (e) {
+    console.error(e);
+}
 
-// REPLACE THE BI QUERY VARIABLES
-const BI_QUERY_VAR_REGEX = /«([^»]+)»/g;
-const variableMatches = sql.match(BI_QUERY_VAR_REGEX);
-const variables = variableMatches
-    ? Object.keys(
-          variableMatches.reduce((matchMap, match) => ({ ...matchMap, [match]: true }), {})
-      ).map(match => ({ match, name: match.slice(1, -1) }))
-    : [];
-const VAR_NAME_SUGGESTIONS = {
-    start_date: /^(date1?|begdate|month)/gi,
-    end_date: /(enddate|edate|date2)/gi,
-    justice: /(justice|judge)/gi,
-};
+function readSource() {
+    const SOURCE_FILE = './source.sql';
+    console.log(`reading sql from '${SOURCE_FILE}'`);
+    return readFileSync(SOURCE_FILE, 'utf8').replace(/;/g, '');
+}
 
-if (variables.length) {
-    console.log(
-        variables.length === 1 ? `found 1 variable` : `found ${variables.length} variables`
-    );
+async function replaceVariables(sourceSql) {
+    const BI_QUERY_VAR_REGEX = /«([^»]+)»/g;
+    const variableMatches = sourceSql.match(BI_QUERY_VAR_REGEX);
+    const variables = variableMatches
+        ? Object.keys(
+              variableMatches.reduce((matchMap, match) => ({ ...matchMap, [match]: true }), {})
+          ).map(match => ({ match, name: match.slice(1, -1) }))
+        : [];
+    const VAR_NAME_SUGGESTIONS = {
+        start_date: /^(date1?|begdate|beginningdate|month)/gi,
+        end_date: /(enddate|edate|date2)/gi,
+        justice: /(justice|judge)/gi,
+    };
 
-    const consoleInterface = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-    const prompt = query => new Promise(resolve => consoleInterface.question(query, resolve));
+    switch (variables.length) {
+        case 0:
+            console.log('no variables found');
+            break;
+        case 1:
+            console.log('found 1 variable');
+            break;
+        default:
+            console.log(`found ${variables.length} found`);
+    }
 
-    for (const { match, name } of variables) {
-        // suggest a new variable name
-        const suggestedName = Object.entries(VAR_NAME_SUGGESTIONS).reduce(
-            (varName, [suggestion, regExp]) => (regExp.test(varName) ? suggestion : varName),
-            name.toLowerCase().replace(/ /g, '_')
-        );
+    let modifiedSql = sourceSql;
+    if (variables.length) {
+        const prompt = query => new Promise(resolve => consoleInterface.question(query, resolve));
 
-        // ask for new var name (or accept suggestion)
-        const newVarName =
-            (await prompt(`${match} variable name (${suggestedName}): `)) || suggestedName;
-        const isDate = newVarName.includes('date'); // maybe prompt user for var type?
-        sql = sql.replaceAll(
-            match,
-            isDate ? `to_date(:${newVarName}, 'mm/dd/yyyy')` : `:${newVarName}`
+        for (const { match, name } of variables) {
+            // suggest a new variable name
+            const suggestedName = Object.entries(VAR_NAME_SUGGESTIONS).reduce(
+                (varName, [suggestion, regExp]) => (regExp.test(varName) ? suggestion : varName),
+                name.toLowerCase().replace(/ /g, '_')
+            );
+
+            // ask for new var name (or accept suggestion)
+            const newVarName =
+                (await prompt(`${match} variable name (${suggestedName}): `)) || suggestedName;
+            const isDate = newVarName.includes('date'); // maybe prompt user for var type?
+            modifiedSql = modifiedSql.replaceAll(
+                match,
+                isDate ? `to_date(:${newVarName}, 'mm/dd/yyyy')` : `:${newVarName}`
+            );
+        }
+
+        consoleInterface.close();
+    }
+    return modifiedSql;
+}
+
+function aliasColumns(sourceSql) {
+    const DESCRIPTION_CODE_REGEX = /([A-Za-z_]+)\.(DESCRIPTION|CODE|CODE_NAME|VOID)(\,?) /gi;
+    const fromIndex = sourceSql.indexOf('from');
+    let selectStatement = sourceSql.slice(0, fromIndex); // only alias the first select statement
+    const restOfSql = sourceSql.slice(fromIndex);
+
+    for (const [fullMatch, tableName, columnName, maybeComma] of selectStatement.matchAll(
+        DESCRIPTION_CODE_REGEX
+    )) {
+        const aliasName = fullMatch.includes('VOID') ? `${tableName}_VOID` : tableName;
+        selectStatement = selectStatement.replace(
+            fullMatch,
+            `${tableName}.${columnName} ${aliasName}${maybeComma} `
         );
     }
 
-    consoleInterface.close();
+    return `${selectStatement} from ${restOfSql}`;
 }
 
-// ALIAS DESCRIPTION COLUMNS
-const DESCRIPTION_CODE_REGEX = /([A-Za-z_]+)\.(DESCRIPTION|CODE|CODE_NAME|VOID)(\,?) /gi;
-let [selectStatement, restOfSql] = sql.split('from'); // only alias the select statement
-for (const [fullMatch, tableName, columnName, maybeComma] of selectStatement.matchAll(
-    DESCRIPTION_CODE_REGEX
-)) {
-    const aliasName = fullMatch.includes('VOID') ? `${tableName}_VOID` : tableName;
-    selectStatement = selectStatement.replace(
-        fullMatch,
-        `${tableName}.${columnName} ${aliasName}${maybeComma} `
-    );
+async function copyToClipboard(sourceSql) {
+    await copy(sqlFormat(sourceSql, { language: 'plsql' }));
+    console.log('formatted sql copied to clipboard');
 }
-
-await copy(sqlFormat(`${selectStatement} from ${restOfSql}`, { language: 'plsql' }));
-console.log('formatted sql copied to clipboard');
